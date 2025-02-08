59cdc402c31e5a53d5aff0c2f0ff43b2d506d16a
VIMABARO <victormabaro@gmail.com>
2025-02-08 00:42:07 -0600

    CICLICAS WHILE Y FOR, EN EL WHILE PUEDES ESTABLECER EL NUMERO PARA DONDE COMENZAR

---
+++ Prog_Basica/Ciclicas_While_For.py
@@ -0,0 +1,13 @@
+#Bucle while
+print("\nINICIO BUCLE WHILE\n")
+contador = int(input("Introduce un número: "))
+while contador <= 10:
+    print(f"Número {contador}")
+    contador += 1
+print("\nFin del bucle while\n")
+
+# Bucle for
+print("\nINICIO BUCLE FOR\n")
+for i in range(1, 6):
+    print(f"Número {i}")
+print("\nFin del bucle FOR\n")
