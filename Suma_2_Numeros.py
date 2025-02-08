36e44212cf0b25956487c51cde8721e7c3a5f22d
VIMABARO <victormabaro@gmail.com>
2025-02-07 21:03:44 -0600

    SUMA DE 2 NUMEROS

---
+++ Suma_2_Numeros.py
@@ -0,0 +1,9 @@
+# Solicitar dos números al usuario
+num1 = int(input("Ingresa el primer número: "))
+num2 = int(input("Ingresa el segundo número: "))
+
+# Realizar la suma
+suma = num1 + num2
+
+# Mostrar el resultado
+print(f"La suma de {num1} y {num2} es: {suma}")
